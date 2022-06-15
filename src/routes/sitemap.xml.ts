e025c65cfc8f66cce6765973857f680addcea2f6
Nuno Góis <github@nunogois.com>
2022-06-15 22:38:51 +0100

    rename dynamic xml routes back to ts

--- src/routes/sitemap.xml.ts
+++ src/routes/sitemap.xml.ts
@@ -2,7 +2,7 @@ import { loadBlog } from './api'
 
 const website = 'https://www.nunogois.com'
 
-export async function get() {
+export async function get(): Promise<unknown> {
 	const posts = await loadBlog()
 	const body = sitemap(posts)
 
