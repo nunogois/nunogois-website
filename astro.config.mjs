7cc3cbb6d6841be07f54a90e4950f922ea22dd77
Nuno Góis <github@nunogois.com>
2023-10-09 18:09:15 +0100

    refactor: specify used icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,35 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import icon from 'astro-icon';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import icon from 'astro-icon'
+import sitemap from '@astrojs/sitemap'
 
-import robotsTxt from "astro-robots-txt";
+import robotsTxt from 'astro-robots-txt'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.nunogois.com',
-  integrations: [tailwind(), icon({
-    include: {
-      'fa6-solid': ['*'],
-      'fa6-brands': ['*']
-    }
-  }), sitemap(), robotsTxt()]
-});
+  integrations: [
+    tailwind(),
+    icon({
+      include: {
+        'fa6-solid': [
+          'at',
+          'briefcase',
+          'check',
+          'code-fork',
+          'code-merge',
+          'code-pull-request',
+          'comment',
+          'globe',
+          'lightbulb',
+          'newspaper',
+          'star',
+          'xmark'
+        ],
+        'fa6-brands': ['dev', 'github', 'linkedin', 'x-twitter']
+      }
+    }),
+    sitemap(),
+    robotsTxt()
+  ]
+})
