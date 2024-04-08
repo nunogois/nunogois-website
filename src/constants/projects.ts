2338198bffc94b5b8697a553827f8de6b03b3be0
Nuno Góis <github@nunogois.com>
2024-04-08 15:15:16 +0100

    update Unleash experience

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -9,10 +9,10 @@ export const EXPERIENCE = [
     url: 'https://www.getunleash.io/',
     role: 'Full Stack Developer (Senior)',
     date: '04/2022 - Current',
-    description: `Contributed to Unleash, an open-source feature flag management solution with over 18M docker pulls, 7K stars on GitHub and a worldwide user base consisting of every kind of end-user, from individual developers to large enterprise companies.
-Integrated with Stripe for billing, involving multiple projects and third party integrations, enabling end-users to sign up and manage their paid plans through their Unleash instance.
-Developed several key features for the product, including Service Accounts, Login History, Groups, Personal Access Tokens (PATs), Environment Cloning, Tag Filtering, Network Visualization, and Search Filtering.
+    description: `Contributed to Unleash, an open-source feature flag management solution with over 21M docker pulls, 10K stars on GitHub and a worldwide user base consisting of more than 100K monthly active end-users, from individual developers to large enterprise companies.
+Assigned to the Enterprise Growth team, developed and often led the development of several key features for the product, including Service Accounts, Signals & Actions, Custom Root Roles, Groups, Personal Access Tokens (PATs), Login History, Multiple Project Roles, Environment Cloning, Tag Filtering, Network Visualization, Slack App, Banners, New Interactive Demo, and Search Filtering.
 Contributed to Unleash Edge, a highly-performant successor to Unleash Proxy written in Rust that allows for a more flexible, performant, resilient and scalable architecture.
+Integrated with Stripe for billing, involving multiple projects and third party integrations, enabling end-users to sign up and manage their paid plans through their Unleash instance.
 Created, developed and maintained multiple SDKs for the product, including Vue.js, Svelte, Solid, and React Native.
 Contributed with ideas and suggestions to the product roadmap, helping to shape the future of the product.
 Collaborated closely with the UI/UX team to identify and implement numerous improvements and fixes that significantly enhanced the end-user experience.
